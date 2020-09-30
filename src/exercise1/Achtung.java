0acac9529dc75182a4cd8539983828e998512c7d
mladenivanov01 <mladenivanov01@gmail.com>
2020-09-30 06:08:10 +0000

    ubung1

---
+++ src/exercise1/Achtung.java
@@ -0,0 +1,14 @@
+package exercise1;
+
+public class Achtung {
+
+  public static void main(String[]args){
+   System.out.println("    *");
+   System.out.println("   ***");
+   System.out.println("  *****");
+   System.out.println(" *******");
+   System.out.println("*********");
+   System.out.println("   ***");
+  }
+
+}
